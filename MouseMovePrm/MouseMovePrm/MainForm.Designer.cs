80b2bbdb6ad2608ce04917b54aa33b1977bfa31e
inho7142 <44658594+inho7142@users.noreply.github.com>
2018-12-06 14:03:17 +0900

    # Project Added # C# MouseMovePrm

---
+++ MouseMovePrm/MouseMovePrm/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace MouseMovePrm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblMousePosition = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 65);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(71, 31);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start (F5)";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(96, 65);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(71, 31);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop (F6)";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(180, 65);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(71, 31);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblMousePosition
+            // 
+            this.lblMousePosition.AutoSize = true;
+            this.lblMousePosition.Location = new System.Drawing.Point(12, 9);
+            this.lblMousePosition.Name = "lblMousePosition";
+            this.lblMousePosition.Size = new System.Drawing.Size(93, 12);
+            this.lblMousePosition.TabIndex = 3;
+            this.lblMousePosition.Text = "Mouse Position";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 99);
+            this.Controls.Add(this.lblMousePosition);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "MainForm";
+            this.Text = "MouseMoveClick";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblMousePosition;
+    }
+}
+
---
+++ MouseMovePrm/MouseMovePrm/MainForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+namespace MouseMovePrm
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("user32.dll",CharSet = CharSet.Auto,CallingConvention = CallingConvention.StdCall)]
+        private static extern void mouse_event(long dwFlags,long dx,long dy,long cButtons,long dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        private Thread _moveThread = null;
+        private bool _isThreadStop = true;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.KeyPreview = true;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartThread();
+        }
+
+        private void StartThread()
+        {
+            if (_moveThread == null)
+            {
+                _isThreadStop = true;
+                _moveThread = new Thread(MouseMovePosition);
+                _moveThread.Start();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            DisposeThread();
+        }
+
+        private void DisposeThread()
+        {
+
+            if (_moveThread != null)
+            {
+                _isThreadStop = false;
+                _moveThread.Join(2000);
+                _moveThread = null;
+            }
+
+            Point position = new Point(this.Location.X - 30 , 0);
+            Click(position);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MouseMovePosition()
+        {
+            Random randomNum = new Random();
+            Point position = this.Location;
+
+            while (_isThreadStop)
+            {
+                CheckForIllegalCrossThreadCalls = false;
+                this.Cursor = new Cursor(Cursor.Current.Handle);
+                Cursor.Clip = new Rectangle(this.Location, this.Size);
+
+                this.Cursor = Cursors.Default;
+                int number = 0;
+
+                if (Cursor.Position.X <= (this.Location.X + this.Width))
+                {
+                    number = randomNum.Next(1, this.Width - 10);
+                    position = new Point(this.Location.X + number, this.Location.Y + (this.Height/2));
+                    Click(position);
+                }
+                else 
+                {
+                    position = new Point(Location.X + 1, this.Location.Y + (this.Height / 2));
+                    Click(position);
+                }
+
+                number = randomNum.Next(1, 3000);
+
+                lblMousePosition.Text =
+                    " Mouse Click Position " + Environment.NewLine
+                    + " SleepTime : " + number.ToString() + Environment.NewLine
+                    + " Mouse PositionX : " + position.X.ToString() + Environment.NewLine
+                    + " Mouse PositionY : " + position.Y.ToString();
+
+                Thread.Sleep(number);
+            }
+        }
+
+        public void Click(Point pt)
+        {
+            Cursor.Position = pt;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, pt.X, pt.Y, 0, 0);
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+            {
+                StartThread();
+                this.Activate();
+            }
+            else if (e.KeyCode == Keys.F6)
+            {
+                DisposeThread();
+                this.Activate();
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisposeThread();
+        }
+    }
+}
